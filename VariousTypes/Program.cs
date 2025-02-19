406ec66b315887860d6c602ce92740a0f2ff84ca
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-19 19:52:52 +0000

    add int

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -4,9 +4,16 @@ namespace VariousTypes
 {
     public class Program
     {
+        /// Program starts here
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int numb_int = 54;
+            int numb_int_hex = 0x6f;
+            int numb_int_bi = 0b1101011;
+
+            Console.WriteLine(numb_int);
+            Console.WriteLine(numb_int_hex);
+            Console.WriteLine(numb_int_bi);
         }
     }
 }
