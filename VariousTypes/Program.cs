58133d6985cac175bb34d94d791c5f333b609087
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-19 20:00:11 +0000

    add char

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -14,6 +14,13 @@ namespace VariousTypes
             Console.WriteLine(numb_int);
             Console.WriteLine(numb_int_hex);
             Console.WriteLine(numb_int_bi);
+
+            char star = '\u2606';
+            char horse = '\u2658';
+            char exclamation2 = '\u203c';
+            Console.WriteLine(star);
+            Console.WriteLine(horse);
+            Console.WriteLine(exclamation2);
         }
     }
 }
