847ba158dcd5b279ec64216e9d7cbc79cddee91c
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-20 19:56:41 +0000

    add input for float

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -9,7 +9,8 @@ namespace AskForInput
         {
             Console.WriteLine("Inserir número intiero");
             int n_int = int.Parse(Console.ReadLine());
-            
+            Console.WriteLine("Inserir um número real");
+            float n_float = float.Parse(Console.ReadLine());
         }
     }
 }
