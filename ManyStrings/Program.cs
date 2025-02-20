d0e1fc17874c98bc7b8523ba8cdf45bbaee421b6
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-20 10:20:37 +0000

    add strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks.Dataflow;
 
 namespace ManyStrings
 {
     public class Program
     {
+        //Program starts here
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string str_1 = "this is a flag\n\u2690";
+            string str_2 = "this is a \b \"test\" to try out \\escape characters\\\r";
+            Console.WriteLine(str_1);
+            Console.WriteLine(str_2);
         }
     }
 }
