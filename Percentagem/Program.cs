1328437a5c09e002772842f95e9fff29ecd0a891
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-21 14:26:43 +0000

    add aligment

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Runtime.Intrinsics.Arm;
 namespace Percentagem
 {
     public class Program
@@ -12,7 +13,10 @@ namespace Percentagem
             float n_float2 = float.Parse(Console.ReadLine());
             float n_float3 = float.Parse(Console.ReadLine());
             float n_float4 = float.Parse(Console.ReadLine());
-           
+           string s1 = $"{n_float1,10}";
+           string s2 = $"{n_float2,10}";
+           string s3 = $"{n_float3,10}";
+           string s4 = $"{n_float4,10}";
         }
     }
 }
