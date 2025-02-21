7e343254df4cdd94d875189193dc289540e239e8
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-21 14:28:07 +0000

    add formating

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -13,10 +13,10 @@ namespace Percentagem
             float n_float2 = float.Parse(Console.ReadLine());
             float n_float3 = float.Parse(Console.ReadLine());
             float n_float4 = float.Parse(Console.ReadLine());
-           string s1 = $"{n_float1,10}";
-           string s2 = $"{n_float2,10}";
-           string s3 = $"{n_float3,10}";
-           string s4 = $"{n_float4,10}";
+           string s1 = $"{n_float1,10:p2}";
+           string s2 = $"{n_float2,10:p2}";
+           string s3 = $"{n_float3,10:p2}";
+           string s4 = $"{n_float4,10:p2}";
         }
     }
 }
