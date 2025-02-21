d33f6f8713281f6df641b3f46687263bb877c480
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-21 14:10:43 +0000

    add float input

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using System.Globalization;
 namespace Percentagem
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            Console.WriteLine("Intruduza 4 números entre 0 e 1, separados por linhas");
+            float n_float1 = float.Parse(Console.ReadLine());
+            float n_float2 = float.Parse(Console.ReadLine());
+            float n_float3 = float.Parse(Console.ReadLine());
+            float n_float4 = float.Parse(Console.ReadLine());
+           
         }
     }
 }
